c4071130a331a3cdb8e0c67856bebfc3509611f8
Konstantin Klimovich <klimovich@playkot.com>
2022-12-06 08:08:39 +0300

    day 6

---
+++ day06/solution.php
@@ -0,0 +1,19 @@
+<?php
+$line = file_get_contents('input.txt');
+$buffer = [];
+
+for ($i = 0, $c = strlen($line); $i < $c; $i++) {
+    $buffer[] = $line[$i];
+    if (count($buffer) > 4) {
+        $buffer = array_splice($buffer, 1);
+    } elseif (count($buffer) < 4) {
+        continue;
+    }
+
+    if (count(array_unique($buffer)) === count($buffer)) {
+        echo $i + 1;
+        break;
+    }
+}
+
+echo PHP_EOL;
